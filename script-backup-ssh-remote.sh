749d5e8ce9cf79d0b07c0a46b3c1591a0d6e56a9
Patricio Balboa <69600943+patobalboa@users.noreply.github.com>
2022-09-01 03:20:26 -0400

    Public Version 2.0

--- script-backup-ssh-remote.sh
+++ script-backup-ssh-remote.sh
@@ -1,51 +1,112 @@
 #!/bin/bash
 # https://github.com/patobalboa
 
+# Backup the postgresql script for remote servers with rsync and mail notification via sendmail.
+#
+# Requirements:
+# - rsync
+# - sendmail
+# - ssh
+# - crontab
+#
+# Usage:
+# ./script-backup-ssh-remote.sh
+#
+# Configuration
+#
+# Set the following variables to your needs:
+# - SENDMAIL
+SENDMAIL_FROM = "backups@domain.com"
+SENDMAIL_TO = "mail@domain.com"
+SENDMAIL_SUBJECT = "Backup from remote server"
+SENDMAIL_BODY_ERROR = "Error while executing backup script"
+SENDMAIL_BODY_SUCCESS = "Backup from remote server successful"
 
-# Set de la ruta en la que guardar los backups
-backup_dir="/tmp/respaldos/"
-# Set fecha para la creación del directorio con el Mes y el Año
-fecha_dir=$(date +%b-%Y/)
-# Set fecha de la creación del archivo .backup
-fecha=$(date +%d-%m-%Y)
-extension=".backup"
-#Set del inicio del nombre del archivo de los backups
-archivo1="database1-"
-archivo2="database2-"
-
-# Creación de la carpeta donde se guardarán los respaldos segun el Mes
-carpeta_date=$backup_dir$fecha_dir
-
-if [ ! -d "$carpeta_date" ];
-then
-    mkdir -p "$carpeta_date"
-fi
-
-#Set del path completo de donde y como se definirá el respaldo
-archivo_db1=$carpeta_date$archivo1$fecha$extension
-archivo_db2=$carpeta_date$archivo2$fecha$extension
-
-# Exportamos la password de la base de datos al enviroment del crontab
+# - POSTGRESQL
 export PGPASSWORD="YOUR_PASSWORD_DATABASE"
+DATABASE_NAME="DATABASE_NAME"
+DATABASE_USERNAME="DATABASE_USERNAME"
+DATABASE_HOST="DATABASE_HOST"
+DATABASE_PORT="DATABASE_PORT"
+DATABASE_BACKUP_PATH="/backup/path"+"-"+$(date +%b-%Y)
+DATABASE_BACKUP_NAME="DATABASE_BACKUP_NAME"+"_"+"$(date +%Y-%m-%d_%H-%M-%S)"
 
-# Añadimos los el comando para la creación de las base de datos.
-pg_dump -h localhost -U postgres -F c -b -v -f $archivo_db1 database1
-pg_dump -h localhost -U postgres -F c -b -v -f $archivo_db2 database2
+# - SSH
+SSH_USERNAME="SSH_USERNAME"
+SSH_HOST="SSH_HOST"
+SSH_PORT="SSH_PORT"
+SSH_BACKUP_PATH="/backup/path"
 
+# - CRONTAB
+CRONTAB_COMMAND="0 0 * * * /path/to/script-backup-ssh-remote.sh"
 
-# Preconfigura tu sendmail para enviar una alerta a traves de un correo.
-# Preguntamos si se creó correctamente el archivo de respaldo y enviaremos un mail si se hizo correctamente o no.
+# - RSYNC
+RSYNC_OPTIONS="-avz --delete"
 
-if [ -f "$archivo_db1" ]; then
-    echo "Subject: Exito: Respaldo Database1 \n\n Respaldo realizado correctamente." | /usr/sbin/sendmail -vf alertas@domain.cl tumail@domain.cl
+# - MAIL
+MAIL_OPTIONS="-f $SENDMAIL_FROM -t $SENDMAIL_TO -s $SENDMAIL_SUBJECT"
+
+# - LOG
+LOG_FILE="/path/to/log/file"
+
+# Functions
+function send_mail {
+    if [ $1 -eq 0 ]; then
+        echo "$SENDMAIL_BODY_SUCCESS" | mail $MAIL_OPTIONS
+    else
+        echo "$SENDMAIL_BODY_ERROR" | mail $MAIL_OPTIONS
+    fi
+}
+
+function log {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") $1" >> $LOG_FILE
+}
+
+function backup_database {
+    log "Backing up database $DATABASE_NAME"
+    pg_dump -U $DATABASE_USERNAME -h $DATABASE_HOST -p $DATABASE_PORT $DATABASE_NAME > $DATABASE_BACKUP_PATH/$DATABASE_BACKUP_NAME.sql
+    if [ $? -eq 0 ]; then
+        log "Database backup successful"
+    else
+        log "Database backup failed"
+    fi
+}
+
+function backup_ssh {
+    log "Backing up ssh"
+    rsync $RSYNC_OPTIONS $SSH_USERNAME@$SSH_HOST:$SSH_BACKUP_PATH $DATABASE_BACKUP_PATH
+    if [ $? -eq 0 ]; then
+        log "SSH backup successful"
+    else
+        log "SSH backup failed"
+    fi
+}
+
+function backup_crontab {
+    log "Backing up crontab"
+    crontab -l > $DATABASE_BACKUP_PATH/$DATABASE_BACKUP_NAME.crontab
+    if [ $? -eq 0 ]; then
+        log "Crontab backup successful"
+    else
+        log "Crontab backup failed"
+    fi
+}
+
+function backup_all {
+    backup_database
+    backup_ssh
+    backup_crontab
+}
+
+# Main
+log "Starting backup"
+backup_all
+if [ $? -eq 0 ]; then
+    log "Backup successful"
 else
-    echo 'Subject: Fallido: Respaldo Database1 \n\n No se hizo el respaldo, Revisar Urgente.' | /usr/sbin/sendmail -vf alertas@domain.cl tumail@domain.cl
+    log "Backup failed"
 fi
-
-pg_dump -h localhost -U postgres -F c -b -v -f $archivo_db2 database2
-
-if [ -f "$archivo_db2" ]; then
-    echo "Subject: Exito: Respaldo Database2 \n\n Respaldo realizado correctamente." | /usr/sbin/sendmail -vf alertas@domain.cl tumail@domain.cl
-else
-    echo 'Subject: Fallido: Respaldo Database2 \n\n No se hizo el respaldo, Revisar Urgente.' | /usr/sbin/sendmail -vf alertas@domain.cl tumail@domain.cl
-fi
+log "Ending backup"
+send_mail $?
+log "Backup finished"
+exit 0
